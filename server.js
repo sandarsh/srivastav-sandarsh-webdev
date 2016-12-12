34441566f31380426924faa131f465635dc1a7ca
sandarsh <sandarshsrivastav@Spirit.local>
2016-12-11 22:08:58 -0500

    Assignment 6 update 3
    Assignment 6 update 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use(bodyParser.json({type : 'application/json'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
